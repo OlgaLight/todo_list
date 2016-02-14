533200f3027516a706d4f9444f49ea8b02baa941
OlgaLight <marshak.oggy@gmail.com>
2016-02-14 11:55:59 +0200

    add DB and validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,24 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validates :gender, inclusion: { in: %w(male female) }
+  validate :names
+  validate :sue_male
+
+
+  def names
+    if first_name.nil? && last_name.nil?
+      errors.add(:first_name, "Please, add your first name or last name")
+    end
+  end
+
+  def sue_male
+    if first_name == "Sue" && gender == "male"
+      errors.add(:gender, "Maybe Sue is female?")
+    end
+  end
+
+  def self.get_all_profiles(min,max)
+    Profile.where("birth_year BETWEEN ? AND ?",min, max).order(birth_year: :asc)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,26 @@
-today = Date.today
-next_date = today + 1.year
-
 User.destroy_all
-TodoList.destroy_all
-TodoItem.destroy_all
 
-user = User.create! [
-                 { username: "Fiorina", password_digest: "pass1" },
-                 { username: "Trump", password_digest: "pass2" },
-                 { username: "Carson", password_digest: "pass3" },
-                 { username: "Clinton", password_digest: "pass4" },
-             ]
+
+due_date = Date.today + 1.year
 
 Profile.create! [
-                    { first_name:"Carly", last_name: "Fiorina", gender: "female", birth_year: 1954, created_at: "", updated_at: "", user_id: User.find_by_username('Fiorina') },
-                    { first_name:"Donald", last_name: "Trump", gender: "male", birth_year: 1946, created_at: "", updated_at: "", user_id: User.find_by_username('Trump') },
-                    { first_name: "Ben", last_name: "Carson", gender: "male", birth_year: 1951, created_at: "", updated_at: "", user_id: User.find_by_username('Carson') },
-                    { first_name: "Hillary", last_name: "Clinton", gender:"female", birth_year: 1947, created_at: "", updated_at: "", user_id: User.find_by_username('Clinton') }
+                    { first_name: "Carly",   last_name: "Fiorina", birth_year: 1954, gender: "female" },
+                    { first_name: "Donald",  last_name: "Trump",   birth_year: 1946, gender: "male" },
+                    { first_name: "Ben",     last_name: "Carson",  birth_year: 1951, gender: "male" },
+                    { first_name: "Hillary", last_name: "Clinton", birth_year: 1947, gender: "female" }
                 ]
 
-TodoList.create! [
-                     { list_name: "Big deal1", list_due_date: next_date, created_at: "", updated_at: "", user: User.find_by_username('Fiorina') },
-                     { list_name: "Big deal2", list_due_date: next_date, created_at: "", updated_at: "", user: User.find_by_username('Trump') },
-                     { list_name: "Big deal3", list_due_date: next_date, created_at: "", updated_at: "", user: User.find_by_username('Carson') },
-                     { list_name: "Big deal4", list_due_date: next_date, created_at: "", updated_at: "", user: User.find_by_username('Clinton') }
-                 ]
+profiles = Profile.all
 
+profiles.each do |profile|
+  profile.create_user( username: profile.last_name, password_digest: profile.birth_year )
+  profile.user.todo_lists.create!(list_name: profile.first_name, list_due_date: due_date )
+end
 
+todo_lists = TodoList.all
 
-(1..5).each do |item|
-  TodoItem.create! [
-                       { title: "Task 1", due_date: next_date, description: "very important task1 #{item}", todo_list_id: (TodoList.find_by(user: User.find_by(username:'Fiorina'))), completed: true },
-                       { title: "Task 2", due_date: next_date, description: "very important task2#{item}", todo_list_id: (TodoList.find_by(user: User.find_by(username: 'Trump'))), completed: true },
-                       { title: "Task 3", due_date: next_date, description: "very important task3#{item}", todo_list_id: (TodoList.find_by(user: User.find_by(username: 'Carson'))), completed: true },
-                       { title: "Task 4", due_date: next_date, description: "very important task4#{item}", todo_list_id: (TodoList.find_by(user: User.find_by(username:'Clinton'))), completed: true }
-                   ]
+todo_lists.each do |list|
+  5.times do
+    list.todo_items.create!(due_date: due_date, title: "Title", description: "Description")
+  end
 end
